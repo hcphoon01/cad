713bc6fbc5d5c05495b38b47800ec2f27988af36
Harry Cameron <hcphoon01@gmail.com>
2019-12-30 20:45:26 +0000

    added home layout and finished dashboard boilerplate

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,21 +23,30 @@
                     <h5 class="card-title">Patrol Numbers</h5>
                     <p class="card-text">1</p>
                 </div>
+                <div class="card-body">
+                    <h5 class="card-title">Fleet Count</h5>
+                    <p class="card-text">50</p>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Total Calls</h5>
+                    <p class="card-text">50</p>
+                </div>
             </div>
         </div>
         <div class="col-6 col-md-4">
             <div class="card flex-grow-1">
                 <h2 class="card-header">Events</h2>
                 <div class="card-body">
-                    <h5 class="card-title">Patrol</h5>
-                    <p class="card-text">Next Patrol</p>
+                    <h5 class="card-title">Daily Patrol</h5>
+                    <p class="card-text">19:00 - 22:00</p>
+                    <a href="#" class="btn btn-success">Book On</a>
                 </div>
             </div>
             <div class="card mt-3 flex-grow-1">
                 <h2 class="card-header">CC's Commendation</h2>
                 <div class="card-body">
-                    <h5 class="card-title">Name: </h5>
-                    <p class="card-text">Commendation: </p>
+                    <h5 class="card-title">Name: H.Cameron</h5>
+                    <p class="card-text">Description: Made ERP such an amazing website</p>
                 </div>
             </div>
         </div>
